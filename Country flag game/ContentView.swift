27aab5410ebdb803ccd7d5c800a174065f8878de
Kat <katherinekelli.creates@gmail.com>
2026-01-06 10:36:13 -0600

    Initial Commit

---
+++ Country flag game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Country flag game
+//
+//  Created by Katherine Pizzirusso on 1/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Country flag game/Country_flag_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_flag_gameApp.swift
+//  Country flag game
+//
+//  Created by Katherine Pizzirusso on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Country_flag_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
