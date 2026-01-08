71c1ac1531979e3bd25f30db4c58bf07ffdcf512
Kat <katherinekelli.creates@gmail.com>
2026-01-08 10:34:31 -0600

    Created custom button

---
+++ Country flag game/Components/CustomButton.swift
@@ -0,0 +1,26 @@
+//
+//  CustomButton.swift
+//  Country flag game
+//
+//  Created by Katherine Pizzirusso on 1/6/26.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var text: String
+    var background: Color = .yellow
+    var body: some View {
+        Text(text)
+            .foregroundColor(.cyan)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(25)
+            .shadow(radius: 10)
+    }
+}
+
+#Preview {
+    CustomButton(text:"Next")
+}
